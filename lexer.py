2535904215ede16238f18b25bf9bd3c37f4c8237
pranithkallakuri <f20180249@hyderabad.bits-pilani.ac.in>
2021-02-14 18:28:00 +0530

    Added basic lexer for Clang

---
+++ lexer.py
@@ -0,0 +1,22 @@
+#Python 3.8.5 64-bit
+#Written by Pranith S Kallakuri
+
+import sys      # This import is required to use commandline argument for filename to be lexed. 
+from tokenizer import Tokenizer, Token
+
+if len(sys.argv) < 2:
+    print("Please enter filename as commandline argument")
+    print("Exiting...")
+    exit()
+
+
+my_tokenizer = Tokenizer()
+my_token_list = my_tokenizer.get_token_list(sys.argv[1])
+
+print()
+print()
+print()
+print()
+
+for token in my_token_list:
+    print("< " + token.token + ", " + token.lexeme + " >")
