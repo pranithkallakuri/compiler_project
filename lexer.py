382ad2cc694d99a08aa2061bcb9b13f5ecccab94
GsvGowtham <69533372+GsvGowtham@users.noreply.github.com>
2021-02-18 13:54:08 +0530

    Add files via upload
    Added Error states

---
+++ lexer.py
@@ -0,0 +1,26 @@
+#Python 3.8.5 64-bit
+#Written by Pranith S Kallakuri
+
+import sys      # This import is required to use commandline argument for filename to be lexed. 
+from tokenizer import Tokenizer, Token
+
+if len(sys.argv) < 2:
+    print("Please enter filename as commandline argument")
+    print("Exiting...")
+    exit()
+
+
+my_tokenizer = Tokenizer(sys.argv[1])
+
+print()
+print()
+print()
+print()
+
+while True:
+    token = my_tokenizer.get_next_token()
+    if token.token == "EOF":
+        break
+    if token.token == "string_error" or token.token=="char_error" or token.token=="float_error" :
+        print("Error encountered at line"+str(token.line))
+    print("< " + token.token + ", " + token.lexeme + " >  b/w indices " + str(token.begin) + " to " + str(token.end) + " on line number " + str(token.line))
